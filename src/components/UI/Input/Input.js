12ee50e8bdc857df54a175e38878f1ae42c0ef31
Anton <aanohin98@gmail.com>
2019-12-20 20:16:57 +0300

    0.6.5
    //In Progress
    
    Create Form

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import cls from './Input.module.css';
+
+
+const Input = props => {
+
+    function isInvalid({valid, touched, shouldValidate}) {
+        return !valid && shouldValidate && touched;
+    }   
+
+    const inputType = props.type || 'text';
+    const clsInput = [cls.Input];
+    const htmlFor = `${inputType}-${Math.random()}`;
+
+    if (isInvalid(props)) {
+        clsInput.push(cls.invalid);
+    }
+
+    return(
+        <div className={clsInput.join(' ')}>
+            <label htmlFor={htmlFor}>{props.label}</label>
+            <input 
+                type={inputType}
+                id={htmlFor}
+                value={props.value}
+                onChange={props.onChange}
+            />
+
+            { isInvalid(props) 
+                ? <span>{props.errorMessage || 'Enter the correct value'}</span>
+                : null
+            }
+            
+        </div>
+    )
+}
+
+export default Input
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import cls from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends Component {
+
+    state = {
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Enter the correct email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Enter the correct password',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
 
     loginHandler = () => {
 
@@ -23,8 +53,8 @@ export default class Auth extends Component {
                     <h1>Authorization</h1>
 
                     <form onSubmit={this.submitHandler} className={cls.AuthForm}>
-                        <input type='text'/>
-                        <input type='text'/>
+                        <Input label="Email" />
+                        <Input label="Password" />
 
                         <Button 
                             type='success' 
